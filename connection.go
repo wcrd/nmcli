644463b1454372cd16f192b964d79a736538f586
wcrd <11898748+wcrd@users.noreply.github.com>
2022-04-01 20:26:16 -0600

    Update Connection structs to simplify functions

--- connection.go
+++ connection.go
@@ -10,26 +10,42 @@ import (
 )
 
 type Connection struct {
-	name      string
-	uuid      string
-	conn_type string
-	device    string
+	Name      string
+	Uuid      string
+	Conn_type string
+	Device    string // aka ifname
+	Addr      *AddressDetails
 }
+
+// Deletes the connection.
+// Returns nmcli success message and error.
+func (c Connection) Delete() (string, error) {
+	res, err := exec.Command("nmcli", "connection", "del", c.Name).Output()
+	if err != nil {
+		return "", err
+	}
+	return string(res), nil
+}
+
+// Modifies the connection with given parameters.
+// func (c Connection) Modify() (string, error)
 
 type NewConnectionDetails struct {
 	Name      string
 	Conn_type string
 	Ifname    string
-	Addr      *NewAddressDetails
+	Addr      *AddressDetails
 }
 
-type NewAddressDetails struct {
+type AddressDetails struct {
 	Ipv4_method  string   `cmd:"ipv4.method"`
 	Ipv4_address string   `cmd:"ipv4.address"`
 	Ipv4_gateway string   `cmd:"ipv4.gateway"`
 	Ipv4_dns     []string `cmd:"ipv4.dns"`
 }
 
+// Returns all connections defined in nmcli
+// Equivalent to: nmcli connection
 func Connections() []Connection {
 	res, err := exec.Command("nmcli", "connection").Output()
 	if err != nil {
@@ -49,13 +65,16 @@ func Connections() []Connection {
 	return results
 }
 
-func GetConnection(conn string) ([]Connection, error) {
+// Finds connection by con-name, if it exists.
+// Returns a list of Connections and an error.
+// Equivalent to: nmcli connection show {name}
+func GetConnectionByName(conn string) ([]Connection, error) {
 	// get connections
 	conns := Connections()
 	// check if connection with name exists
 	existingConns := []Connection{}
 	for _, c := range conns {
-		if c.name == conn {
+		if c.Name == conn {
 			existingConns = append(existingConns, c)
 		}
 	}
@@ -70,32 +89,28 @@ func GetConnection(conn string) ([]Connection, error) {
 	}
 }
 
-func AddConnection(conn *NewConnectionDetails) error {
-	// Check if connection exists
-
+// Creates a new connection
+// Equivalent to: nmcli con add con-name {name} type {type} ifname {ifname}
+// Returns nmcli message and error
+func AddConnection(conn *Connection) (string, error) {
 	// Create new connection
 	// TODO: Is it worth doing this in two parts? Or should execute as one command?
-	_, err := exec.Command("nmcli", "connection", "add", "con-name", conn.Name, "type", conn.Conn_type, "ifname", conn.Ifname).Output()
+	res, err := exec.Command("nmcli", "connection", "add", "con-name", conn.Name, "type", conn.Conn_type, "ifname", conn.Device).Output()
 	if err != nil {
-		// handle error
-		fmt.Println(err)
-		return err
+		return string(res), err
 	}
 
 	// Update connection with address details
 	cmds := conn.Addr.construct_commands()
 	fmt.Println(append([]string{"connection", "mod", conn.Name}, cmds...))
-	res, err := exec.Command("nmcli", append([]string{"connection", "mod", conn.Name}, cmds...)...).CombinedOutput()
+	res, err = exec.Command("nmcli", append([]string{"connection", "mod", conn.Name}, cmds...)...).CombinedOutput()
 	if err != nil {
-		// handle error
-		fmt.Println(err)
-		fmt.Printf("%s\n", res)
-		return err
+		return string(res), err
 	}
-	return nil
+	return string(res), nil
 }
 
-func (addr *NewAddressDetails) construct_commands() []string {
+func (addr *AddressDetails) construct_commands() []string {
 	output := make([]string, 0)
 	// Get type
 	t := reflect.TypeOf(*addr)
@@ -142,10 +157,10 @@ func parseConnection(conn_line string) Connection {
 	}
 
 	return Connection{
-		name:      strings.TrimSpace(match[1]),
-		uuid:      strings.TrimSpace(match[2]),
-		conn_type: strings.TrimSpace(match[3]),
-		device:    strings.TrimSpace(match[4]),
+		Name:      strings.TrimSpace(match[1]),
+		Uuid:      strings.TrimSpace(match[2]),
+		Conn_type: strings.TrimSpace(match[3]),
+		Device:    strings.TrimSpace(match[4]),
 	}
 }
 
